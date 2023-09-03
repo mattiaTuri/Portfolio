e9b4ac0aa76cd66de52d61e9d8b98626aba26902
mattiaTuri <turina.mattia@gmail.com>
2023-09-03 12:42:32 +0200

    insert new project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,73 +2,40 @@ import { Project } from "@/models/project";
 import leagueoflegends from "../assets/images/leagueoflegends.jpg"
 import worldjournal from "../assets/images/worldjournal.png"
 import dashboard from "../assets/images/dashboard.png"
-
+import sortingalgorithms from "../assets/images/sorting-algorithms.jpg"
 
 export const projects: Project[] = [
     {
-        id: "journal",
-        title: "World Jounal",
-        subtitle: "A clone of the New York Times",
-        description:"The webpage provides up-to-date newspaper news on various topics. The main page of every sections  provides a quick overview of the news, with captivating headlines and brief excerpts that capture the essence of each article. Users can click on articles of their interest to read the entire content and stay informed about the latest updates.",
-        link_github:"https://github.com/mattiaTuri/Journal",
-        link_preview:"https://world-journal-react.netlify.app/",
+        id: "sorting-algorithms",
+        title: "Sorting Algorithms",
+        subtitle: "Visualizer of the main sorting algorithms",
+        description:"Visualizer of the main sorting algorithms: bubble, insertion, selection, merge, quick. Logic and animations made with Javascript. On the page is include a toolbar which is possible increase and decrease the speed and also change the length of array.",
+        link_github:"https://github.com/mattiaTuri/SortingAlgorithms",
+        link_preview:"https://sorting-algorithms-five.vercel.app/",
         technology: [
             {
-                name: "React v.17",
+                name: "React v.18.2",
                 icon: "mdi:react"
+            },
+            {
+                name: "Javascript",
+                icon: "tabler:brand-javascript"
             },
             {
                 name: "MUI",
                 icon: "simple-icons:mui"
             },
             {
-                name: "React router",
-                icon: "simple-icons:reactrouter"
-            },
-            {
-                name: "Axios",
-                icon: "simple-icons:axios"
-            },
-        ],
-        img: worldjournal,
-        imgPosition: "center"
-    },
-    {
-        id: "league-of-legends",
-        title: "League of legends",
-        subtitle: "The universe of the most famous MOBA",
-        description:"The webpage is a comprehensive encyclopedia about the League of Legends universe, featuring details about champions and regions. It provides in-depth information about playable characters such as their available skins, story and others.",
-        link_github:"https://github.com/mattiaTuri/LeagueOfLegends",
-        link_preview:"https://league-of-legends-universe.vercel.app",
-        technology: [
-            {
-                name: "Next.js v.13",
-                icon: "teenyicons:nextjs-solid"
-            },
-            {
-                name: "Typescript",
-                icon: "mdi:language-typescript"
-            },
-            {
                 name: "Tailwind css",
                 icon: "mdi:tailwind"
             },
             {
-                name: "i18next",
-                icon: "simple-icons:i18next"
-            },
-            {
-                name: "Framer motion",
-                icon: "tabler:brand-framer-motion"
-            },
-
-            {
-                name: "Swiper.js",
-                icon: "simple-icons:swiper"
+                name: "React router",
+                icon: "simple-icons:reactrouter"
             },
         ],
-        img: leagueoflegends,
-        imgPosition: "40% 20%"
+        img: sortingalgorithms,
+        imgPosition: "top"
     },
     {
         id: "storage",
@@ -113,6 +80,71 @@ export const projects: Project[] = [
 
         ],
         img: dashboard,
+        imgPosition: "center"
+    },
+    {
+        id: "league-of-legends",
+        title: "League of legends",
+        subtitle: "The universe of the most famous MOBA",
+        description:"The webpage is a comprehensive encyclopedia about the League of Legends universe, featuring details about champions and regions. It provides in-depth information about playable characters such as their available skins, story and others.",
+        link_github:"https://github.com/mattiaTuri/LeagueOfLegends",
+        link_preview:"https://league-of-legends-universe.vercel.app",
+        technology: [
+            {
+                name: "Next.js v.13",
+                icon: "teenyicons:nextjs-solid"
+            },
+            {
+                name: "Typescript",
+                icon: "mdi:language-typescript"
+            },
+            {
+                name: "Tailwind css",
+                icon: "mdi:tailwind"
+            },
+            {
+                name: "i18next",
+                icon: "simple-icons:i18next"
+            },
+            {
+                name: "Framer motion",
+                icon: "tabler:brand-framer-motion"
+            },
+
+            {
+                name: "Swiper.js",
+                icon: "simple-icons:swiper"
+            },
+        ],
+        img: leagueoflegends,
+        imgPosition: "40% 20%"
+    },
+    {
+        id: "journal",
+        title: "World Jounal",
+        subtitle: "A clone of the New York Times",
+        description:"The webpage provides up-to-date newspaper news on various topics. The main page of every sections  provides a quick overview of the news, with captivating headlines and brief excerpts that capture the essence of each article. Users can click on articles of their interest to read the entire content and stay informed about the latest updates.",
+        link_github:"https://github.com/mattiaTuri/Journal",
+        link_preview:"https://world-journal-react.netlify.app/",
+        technology: [
+            {
+                name: "React v.17",
+                icon: "mdi:react"
+            },
+            {
+                name: "MUI",
+                icon: "simple-icons:mui"
+            },
+            {
+                name: "React router",
+                icon: "simple-icons:reactrouter"
+            },
+            {
+                name: "Axios",
+                icon: "simple-icons:axios"
+            },
+        ],
+        img: worldjournal,
         imgPosition: "center"
     },
 ]
